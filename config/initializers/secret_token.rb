e4733b6e594da7e3924e65d9ad6e5a0d43184588
Matthew Braden <m.w.braden@gmail.com>
2012-03-25 13:50:00 -0500

    Template Upload

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OMGtest::Application.config.secret_token = '54a2ae09ea361bc33a7a17e305c50c7b817dcbc155cdf8d448acb0de887896d997322860716abafbc5c93596b1b0e2c704acfe73ed123c85587a32fbf4e75542'
